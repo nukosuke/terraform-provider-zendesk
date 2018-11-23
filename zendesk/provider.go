9fc5b0ca7b98a6b5bcb533284db6c7d0151a5f1b
nukosuke <nukosuke@lavabit.com>
2018-11-24 03:30:51 +0900

    Add ConfigureFunc

--- zendesk/provider.go
+++ zendesk/provider.go
@@ -2,6 +2,7 @@ package zendesk
 
 import (
 	"github.com/hashicorp/terraform/helper/schema"
+	client "github.com/nukosuke/go-zendesk/zendesk"
 )
 
 // Provider returns provider instance for Zendesk
@@ -31,5 +32,28 @@ func Provider() *schema.Provider {
 			"zendesk_ticket_form":  resourceZendeskTicketForm(),
 			"zendesk_trigger":      resourceZendeskTrigger(),
 		},
+
+		ConfigureFunc: providerConfigure,
 	}
+}
+
+func providerConfigure(d *schema.ResourceData) (interface{}, error) {
+	config := Config{
+		Account: d.Get("account").(string),
+		Email:   d.Get("email").(string),
+		Token:   d.Get("token").(string),
+	}
+
+	// Create & configure Zendesk API client
+	zd, err := client.NewClient(nil)
+	if err != nil {
+		return nil, err
+	}
+
+	if err = zd.SetSubdomain(config.Account); err != nil {
+		return nil, err
+	}
+	zd.SetCredential(client.NewAPITokenCredential(config.Email, config.Token))
+
+	return zd, nil
 }
