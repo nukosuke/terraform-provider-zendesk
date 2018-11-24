e3c30d202fe7a0f9f7be08100ab842cb3973add5
nukosuke <nukosuke@lavabit.com>
2018-11-24 17:52:17 +0900

    Add ticket form which refer ticket fields

--- examples/ticket_forms.tf
+++ examples/ticket_forms.tf
@@ -6,6 +6,22 @@
 #
 # (C) 2018 nukosuke <nukosuke@lavabit.com>
 
-resource "zendesk_ticket_form" "1-form" {
+resource "zendesk_ticket_form" "form-1" {
   name = "Form 1"
+  ticket_field_ids = [
+    "${zendesk_ticket_field.checkbox-field.id}",
+    "${zendesk_ticket_field.date-field.id}",
+    "${zendesk_ticket_field.decimal-field.id}",
+    "${zendesk_ticket_field.integer-field.id}",
+  ]
+}
+
+resource "zendesk_ticket_form" "form-2" {
+  name = "Form 2"
+  ticket_field_ids = [
+    "${zendesk_ticket_field.regexp-field.id}",
+    "${zendesk_ticket_field.tagger-field.id}",
+    "${zendesk_ticket_field.text-field.id}",
+    "${zendesk_ticket_field.textarea-field.id}",
+  ]
 }
