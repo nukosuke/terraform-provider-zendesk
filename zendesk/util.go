6ebb5a4da754b53b754edd90fb1eaf4e951fa931
Andy McCall <andymc903@gmail.com>
2019-03-10 17:42:37 -0400

    adding testing utilities

--- zendesk/util.go
+++ zendesk/util.go
@@ -1,8 +1,49 @@
 package zendesk
 
-import "github.com/hashicorp/terraform/helper/schema"
+type getter interface {
+	Get(string) interface{}
+}
 
-func setSchemaFields(d *schema.ResourceData, m map[string]interface{}) error {
+type setter interface {
+	Set(string, interface{}) error
+}
+
+type identifiable interface {
+	Id() string
+}
+
+type identifiableGetterSetter interface {
+	identifiable
+	getter
+	setter
+}
+
+type mapGetterSetter map[string]interface{}
+
+func (m mapGetterSetter) Get(k string) interface{} {
+	v, ok := m[k]
+	if !ok {
+		return nil
+	}
+
+	return v
+}
+
+func (m mapGetterSetter) Set(k string, v interface{}) error {
+	m[k] = v
+	return nil
+}
+
+type identifiableMapGetterSetter struct {
+	mapGetterSetter
+	id string
+}
+
+func (i identifiableMapGetterSetter) Id() string {
+	return i.id
+}
+
+func setSchemaFields(d setter, m map[string]interface{}) error {
 	for k, v := range m {
 		err := d.Set(k, v)
 		if err != nil {
