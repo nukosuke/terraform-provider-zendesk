0197c05a3665001564f0862b7272fcb84d284c59
Andy McCall <andymc903@gmail.com>
2019-03-12 10:24:57 -0400

    removed required

--- zendesk/resource_zendesk_ticket_field.go
+++ zendesk/resource_zendesk_ticket_field.go
@@ -116,11 +116,11 @@ func resourceZendeskTicketField() *schema.Resource {
 					Schema: map[string]*schema.Schema{
 						"name": {
 							Type:     schema.TypeString,
-							Required: true,
+							Optional: true,
 						},
 						"value": {
 							Type:     schema.TypeString,
-							Required: true,
+							Optional: true,
 						},
 					},
 				},
