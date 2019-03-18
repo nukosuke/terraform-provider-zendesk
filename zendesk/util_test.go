7fcdc431cc7efe6425551b49fe344c688510bca1
Andy McCall <andymc903@gmail.com>
2019-03-17 20:03:17 -0400

    added test for validation func

---
+++ zendesk/util_test.go
@@ -0,0 +1,16 @@
+package zendesk
+
+import "testing"
+
+func TestIsValidFile(t *testing.T) {
+	v := isValidFile()
+	_, errs := v("testdata/street.jpg", "file_path")
+	if len(errs) != 0 {
+		t.Fatalf("is Valid returned an error")
+	}
+
+	_, errs = v("Missing", "file_path")
+	if len(errs) == 0 {
+		t.Fatalf("is Valid did not return an error")
+	}
+}
