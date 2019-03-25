7a1cf969fb3d1b75758ed9faf1ca156673310937
Andy McCall <andymc903@gmail.com>
2019-03-25 15:27:19 -0400

    started on acceptance tests

--- zendesk/resource_zendesk_attachment_test.go
+++ zendesk/resource_zendesk_attachment_test.go
@@ -1,14 +1,27 @@
 package zendesk
 
 import (
+	"crypto/sha1"
+	"encoding/hex"
+	"fmt"
 	"io"
 	"io/ioutil"
+	"os"
 	"testing"
 
 	. "github.com/golang/mock/gomock"
+	"github.com/hashicorp/terraform/helper/resource"
 	"github.com/nukosuke/go-zendesk/zendesk"
 	"github.com/nukosuke/go-zendesk/zendesk/mock"
 )
+
+const attachmentConfig = `
+resource "zendesk_attachment" "file" {
+  file_name = "street.jpg"
+  file_path = "%s"
+  file_hash = "%s"
+}
+`
 
 type mockUploadWriter struct {
 	io.Writer
@@ -63,4 +76,53 @@ func TestCreateZendeskAttachment(t *testing.T) {
 	if v := d.Id(); v != "1234" {
 		t.Fatalf("Resource did not have expected id %s", v)
 	}
+}
+
+func TestAccZendeskAttachment(t *testing.T) {
+	original, err := os.Open("testdata/street.jpg")
+	if err != nil {
+		t.Fatalf("could not open street file")
+	}
+	defer original.Close()
+
+	h := sha1.New()
+	tee := io.TeeReader(original, h)
+
+	tmpfile, err := ioutil.TempFile("", "new-streets.jpg")
+	if err != nil {
+		t.Fatalf("could not create temp file")
+	}
+
+	defer os.Remove(tmpfile.Name())
+	defer tmpfile.Close()
+
+	_, err = io.Copy(tmpfile, tee)
+	if err != nil {
+		t.Fatalf("error creating temp file: %v", err)
+	}
+
+	hashString := hex.EncodeToString(h.Sum(nil))
+
+	resource.Test(t, resource.TestCase{
+		PreCheck: func() {
+			testAccPreCheck(t)
+		},
+		Providers: testAccProviders,
+		//TODO: check destroyed
+		Steps: []resource.TestStep{
+			{
+				Config: fmt.Sprintf(attachmentConfig, original.Name(), hashString),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttrSet("zendesk_attachment.file", "content_url"),
+				),
+			},
+			{
+				Config: fmt.Sprintf(attachmentConfig, tmpfile.Name(), hashString),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttrSet("zendesk_attachment.file", "content_url"),
+				),
+			},
+		},
+	})
+
 }
