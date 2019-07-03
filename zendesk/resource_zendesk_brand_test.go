087619a614cc6696ea9f4adc9aad70ea1ef6d7aa
Andy McCall <tmccall@zuora.com>
2019-07-03 10:00:59 -0400

    added test for read brand

--- zendesk/resource_zendesk_brand_test.go
+++ zendesk/resource_zendesk_brand_test.go
@@ -8,34 +8,34 @@ import (
 	"testing"
 )
 
+var testBrand = zendesk.Brand{
+	ID:              47,
+	URL:             "https://company.zendesk.com/api/v2/brands/47.json",
+	Name:            "Brand 1",
+	BrandURL:        "https://brand1.com",
+	HasHelpCenter:   true,
+	HelpCenterState: "enabled",
+	Active:          true,
+	Default:         true,
+	Logo: zendesk.Attachment{
+		ID:          928374,
+		FileName:    "brand1_logo.png",
+		ContentURL:  "https://company.zendesk.com/logos/brand1_logo.png",
+		ContentType: "image/png",
+		Size:        166144,
+	},
+	Subdomain:         "brand1",
+	HostMapping:       "brand1.com",
+	SignatureTemplate: "{{agent.signature}}",
+}
+
 func TestCreateBrand(t *testing.T) {
 	ctrl := NewController(t)
 	defer ctrl.Finish()
 
 	m := mock.NewClient(ctrl)
 
-	b := zendesk.Brand{
-		ID:              47,
-		URL:             "https://company.zendesk.com/api/v2/brands/47.json",
-		Name:            "Brand 1",
-		BrandURL:        "https://brand1.com",
-		HasHelpCenter:   true,
-		HelpCenterState: "enabled",
-		Active:          true,
-		Default:         true,
-		Logo: zendesk.Attachment{
-			ID:          928374,
-			FileName:    "brand1_logo.png",
-			ContentURL:  "https://company.zendesk.com/logos/brand1_logo.png",
-			ContentType: "image/png",
-			Size:        166144,
-		},
-		Subdomain:         "brand1",
-		HostMapping:       "brand1.com",
-		SignatureTemplate: "{{agent.signature}}",
-	}
-
-	m.EXPECT().CreateBrand(Any()).Return(b, nil)
+	m.EXPECT().CreateBrand(Any()).Return(testBrand, nil)
 
 	i := newIdentifiableGetterSetter()
 	err := createBrand(i, m)
@@ -43,12 +43,30 @@ func TestCreateBrand(t *testing.T) {
 		t.Fatalf("Create brand returned an error %v", err)
 	}
 
-	if i.Id() != fmt.Sprintf("%d", b.ID) {
-		t.Fatalf("Created object does not have the correct brand id. Was: %s. Expected %d", i.Id(), b.ID)
+	if i.Id() != fmt.Sprintf("%d", testBrand.ID) {
+		t.Fatalf("Created object does not have the correct brand id. Was: %s. Expected %d", i.Id(), testBrand.ID)
 	}
 
-	if i.Get("logo_attachment_id") != b.Logo.ID {
-		t.Fatalf("Created object does not have the correct logo id. Was: %d. Expected %d", i.Get("logo_attachment_id"), b.Logo.ID)
+	if i.Get("logo_attachment_id") != testBrand.Logo.ID {
+		t.Fatalf("Created object does not have the correct logo id. Was: %d. Expected %d", i.Get("logo_attachment_id"), testBrand.Logo.ID)
 	}
-
 }
+
+func TestReadBrand(t *testing.T) {
+	ctrl := NewController(t)
+	defer ctrl.Finish()
+
+	m := mock.NewClient(ctrl)
+	m.EXPECT().GetBrand(testBrand.ID).Return(testBrand, nil)
+	i := newIdentifiableGetterSetter()
+	i.SetId(fmt.Sprintf("%d", testBrand.ID))
+
+	err := readBrand(i, m)
+	if err != nil {
+		t.Fatalf("readBrand returned an error: %v", err)
+	}
+
+	if v := i.Get("subdomain"); v != testBrand.Subdomain {
+		t.Fatalf("Subdomain was not set to the expected value. Was: %s Expected %s", v, testBrand.Subdomain)
+	}
+ }
