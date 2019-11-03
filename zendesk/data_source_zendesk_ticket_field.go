959a321aaea2b93986b783c4301e0360aef1a734
Justin Salmon <Justin.Salmon@10XBANKING.COM>
2019-11-03 14:31:47 +0000

    Upgrade to latest terraform provider SDK

--- zendesk/data_source_zendesk_ticket_field.go
+++ zendesk/data_source_zendesk_ticket_field.go
@@ -1,7 +1,7 @@
 package zendesk
 
 import (
-	"github.com/hashicorp/terraform/helper/schema"
+	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
 	"github.com/nukosuke/go-zendesk/zendesk"
 	"strconv"
 )
