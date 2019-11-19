ddc903746383cdaab0e4cc17587a715438e7c9b7
nukosuke <nukosuke@users.noreply.github.com>
2019-11-19 12:10:14 +0900

    Migrate example tf files to HCL2

--- examples/zendesk.tf
+++ examples/zendesk.tf
@@ -7,12 +7,12 @@
 # NOTE:
 #   v0.0.0 supports only token authentication
 
-variable "ZENDESK_ACCOUNT" { type = "string" }
-variable "ZENDESK_EMAIL"   { type = "string" }
-variable "ZENDESK_TOKEN"   { type = "string" }
+variable "ZENDESK_ACCOUNT" { type = string }
+variable "ZENDESK_EMAIL"   { type = string }
+variable "ZENDESK_TOKEN"   { type = string }
 
 provider "zendesk" {
-  account = "${var.ZENDESK_ACCOUNT}"
-  email   = "${var.ZENDESK_EMAIL}"
-  token   = "${var.ZENDESK_TOKEN}"
+  account = var.ZENDESK_ACCOUNT
+  email   = var.ZENDESK_EMAIL
+  token   = var.ZENDESK_TOKEN
 }
