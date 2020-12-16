86c7acd0855651e2e9eb666a13bef54ca924b90b
nukosuke <nukosuke@users.noreply.github.com>
2020-12-16 13:14:57 +0900

    go fmt

--- main.go
+++ main.go
@@ -1,8 +1,8 @@
 package main
 
 import (
-	"github.com/hashicorp/terraform-plugin-sdk/v2/plugin"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/plugin"
 	"github.com/nukosuke/terraform-provider-zendesk/zendesk"
 )
 
