8c402228708c7e094cd42672697a345d897eb8d5
nukosuke <nukosuke@users.noreply.github.com>
2022-04-20 00:29:31 +0900

    Add dynamic content resource

---
+++ zendesk/resource_zendesk_dynamic_content.go
@@ -0,0 +1,128 @@
+package zendesk
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+	client "github.com/nukosuke/go-zendesk/zendesk"
+)
+
+// https://developer.zendesk.com/api-reference/ticketing/ticket-management/dynamic_content/
+func resourceZendeskDynamicContentItem() *schema.Resource {
+	return &schema.Resource{
+		Create: func(d *schema.ResourceData, meta interface{}) error {
+			zd := meta.(*client.Client)
+			return createDynamicContentItem(d, zd)
+		},
+		Schema: map[string]*schema.Schema{
+			"name": {
+				Type:     schema.TypeString,
+				Required: true,
+			},
+			"default_locale": {
+				// TODO: convert locale_id (int64) to locale string using zendesk.LocaleType
+				Type:     schema.TypeInt,
+				Required: true,
+			},
+			"variant": {
+				Type: schema.TypeSet,
+				Elem: &schema.Resource{
+					Schema: map[string]*schema.Schema{
+						"active": {
+							Type:     schema.TypeBool,
+							Optional: true,
+							Default:  true,
+						},
+						"content": {
+							Type:     schema.TypeString,
+							Required: true,
+						},
+						// TODO: convert locale_id (int64) to locale string using zendesk.LocaleType
+						"locale": {
+							Type:     schema.TypeString,
+							Required: true,
+						},
+					},
+				},
+			},
+		},
+	}
+}
+
+func marshalDynamicContentItem(item client.DynamicContentItem, d identifiableGetterSetter) error {
+	fields := map[string]interface{}{
+		"name":           item.Name,
+		"default_locale": item.DefaultLocaleID,
+	}
+
+	var variants []map[string]interface{}
+	for _, v := range item.Variants {
+		m := map[string]interface{}{
+			"active":  v.Active,
+			"content": v.Content,
+			"locale":  v.LocaleID,
+		}
+		variants = append(variants, m)
+	}
+	fields["variant"] = variants
+
+	return setSchemaFields(d, fields)
+}
+
+func unmarshalDynamicContentItem(d identifiableGetterSetter) (client.DynamicContentItem, error) {
+	item := client.DynamicContentItem{}
+
+	if v := d.Id(); v != "" {
+		id, err := atoi64(v)
+		if err != nil {
+			return item, fmt.Errorf("could not parse dynamic content item id %s: %v", v, err)
+		}
+		item.ID = id
+	}
+
+	if v, ok := d.GetOk("name"); ok {
+		item.Name = v.(string)
+	}
+
+	if v, ok := d.GetOk("default_locale"); ok {
+		// FIXME: see TODO above
+		item.DefaultLocaleID = v.(int64)
+	}
+
+	if v, ok := d.GetOk("variant"); ok {
+		tfstateVariants := v.(*schema.Set).List()
+		variants := []client.DynamicContentVariant{}
+
+		for _, vari := range tfstateVariants {
+			variant, ok := vari.(map[string]interface{})
+			if !ok {
+				return item, fmt.Errorf("could not parse 'variant' for dynamic content item %v", item)
+			}
+			variants = append(variants, client.DynamicContentVariant{
+				Active:   variant["active"].(bool),
+				Content:  variant["content"].(string),
+				LocaleID: variant["locale"].(int64),
+			})
+		}
+		item.Variants = variants
+	}
+
+	return item, nil
+}
+
+func createDynamicContentItem(d identifiableGetterSetter, zd client.DynamicContentAPI) error {
+	item, err := unmarshalDynamicContentItem(d)
+	if err != nil {
+		return err
+	}
+
+	ctx := context.Background()
+	item, err = zd.CreateDynamicContentItem(ctx, item)
+	if err != nil {
+		return err
+	}
+
+	d.SetId(fmt.Sprintf("%d", item.ID))
+	return marshalDynamicContentItem(item, d)
+}
