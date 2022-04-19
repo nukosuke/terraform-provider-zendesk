c24c29dd2c7e8c7736ebd4f5cce713d4b32c3259
nukosuke <nukosuke@users.noreply.github.com>
2022-04-20 04:28:04 +0900

    Add resource .tf

---
+++ examples/dynamic_content_items.tf
@@ -0,0 +1,26 @@
+# dynamic_content_items.tf
+#   Dynamic content item example
+#
+# API reference:
+#   https://developer.zendesk.com/api-reference/ticketing/ticket-management/dynamic_content/
+
+resource "zendesk_dynamic_content_item" "loc-lang" {
+  name            = "language"
+  default_locale  = "en-us"
+
+  variant {
+    locale  = "en-us"
+    content = "English (US)"
+  }
+
+  variant {
+    locale  = "ja"
+    content = "日本語"
+  }
+
+  variant {
+    active  = false
+    locale  = "zh-tw"
+    content = "繁體中文"
+  }
+}
