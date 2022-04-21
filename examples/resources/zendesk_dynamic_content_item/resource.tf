6d69e6ce49477ccfc44cc28d50e85268c4cef2e1
nukosuke <nukosuke@users.noreply.github.com>
2022-04-22 03:08:45 +0900

    Fix dynamic content item resource

--- examples/resources/zendesk_dynamic_content_item/resource.tf
+++ examples/resources/zendesk_dynamic_content_item/resource.tf
@@ -2,21 +2,21 @@
 #   https://developer.zendesk.com/api-reference/ticketing/ticket-management/dynamic_content/
 
 resource "zendesk_dynamic_content_item" "loc-lang" {
-  name            = "language"
-  default_locale  = "en-us"
+  name           = "language"
+  default_locale = "en-US"
 
   variant {
-    locale  = "en-us"
+    locale  = "en-US"
     content = "English (US)"
   }
 
   variant {
     locale  = "ja"
     content = "日本語"
+    default = true
   }
 
   variant {
-    active  = false
     locale  = "zh-tw"
     content = "繁體中文"
   }
